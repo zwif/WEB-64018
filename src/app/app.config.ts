3d988012dbfb7d05cd4a951c3a332070d50f18e2
Johannes Hager <jhager@lorenz.cc>
2023-11-22 11:58:05 +0100

    inital store setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,10 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideStore } from '@ngrx/store';
+import { provideState, provideStore } from '@ngrx/store';
 
 import { routes } from './app.routes';
+import { reducer } from "./reducers";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideStore()]
+  providers: [provideRouter(routes), provideStore(), provideState({name: 'game', reducer: reducer})]
 };
--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,19 +1,23 @@
-import { isDevMode } from '@angular/core';
-import {
-  ActionReducer,
-  ActionReducerMap,
-  createFeatureSelector,
-  createSelector,
-  MetaReducer
+import {createReducer,
+  on
 } from '@ngrx/store';
 
-export interface State {
+import * as Actions from '../actions';
 
+export interface State {
+  home: number
+  away: number;
 }
 
-export const reducers: ActionReducerMap<State> = {
+const initialState: State = {
+  home: 0,
+  away: 0,
+}
 
-};
-
-
-export const metaReducers: MetaReducer<State>[] = isDevMode() ? [] : [];
+export const reducer = createReducer(
+  initialState,
+  on(Actions.homeScore, state => ({ ...state, home: state.home + 1 })),
+  on(Actions.awayScore, state => ({ ...state, away: state.away + 1 })),
+  on(Actions.resetScore, state => ({ home: 0, away: 0 })),
+  on(Actions.setScores, (state, { game }) => ({ home: game.home, away: game.away }))
+)

afa4f85454e3d2b36ee69ee35e63270e93425595
Johannes Hager <jhager@lorenz.cc>
2023-11-22 11:28:38 +0100

    add ngrx dependency

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideStore } from '@ngrx/store';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideStore()]
 };
